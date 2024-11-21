f0041019a4e598751be8af89400b28e593bc5ecd
Aaron Smith <aaronsmith1234@gmail.com>
2024-11-20 21:49:09 -0500

    add reqs

--- example_package/expected_benefits.py
+++ example_package/expected_benefits.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def determine_expected_benefit_for_one_person(
     age: int, gender: str, benefit_amt: int, mort_table: pd.DataFrame = None
-):
+) -> float:
     # load mortality tables if not provided
     if mort_table is None:
         male_mort = get_mortality_data(
