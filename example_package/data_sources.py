9847c6bb367506c863ca223373d2c0b2e1bfb1a5
acggit <antoncotelo@gmail.com>
2024-11-27 11:40:25 -0800

    improved get_mortality_data

--- example_package/data_sources.py
+++ example_package/data_sources.py
@@ -1,16 +1,19 @@
 import pandas as pd
+import xlwings as xw
 
 
 def get_mortality_data(gender: str) -> pd.DataFrame:
+    """Load mortality table from public URL and return dataframe."""
     if gender.lower() == "male":
-        table_id = "M"
+        gender_code = "M"
     elif gender.lower() == "female":
-        table_id = "F"
+        gender_code = "F"
     else:
         raise KeyError("You've entered a gender for which there is no table")
-
     # https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables2017.html
-    return pd.read_csv(
-        f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{table_id}_Hist_TR2017.csv",
+    df = pd.read_csv(
+        f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{gender_code}_Hist_TR2017.csv",
         skiprows=4,
     )
+    xw.view(df)
+    return df
