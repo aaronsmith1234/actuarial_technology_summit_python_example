59459335edb373026046a0e507db8ecd5dbfd5e7
aaronsmith1234 <aaronsmith1234@users.noreply.github.com>
2024-11-26 23:23:08 -0500

    update notebooks (#4)
    Co-authored-by: s005158 <s005158@standard.com>

--- example_package/data_sources.py
+++ example_package/data_sources.py
@@ -8,6 +8,7 @@ def get_mortality_data(gender: str) -> pd.DataFrame:
         table_id = "F"
     else:
         raise KeyError("You've entered a gender for which there is no table")
+
     # https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables2017.html
     return pd.read_csv(
         f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{table_id}_Hist_TR2017.csv",
