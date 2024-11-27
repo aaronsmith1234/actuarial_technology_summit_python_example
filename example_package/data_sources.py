8eafdb33e71235851ed0687ebf2ea3853e12c8c4
acggit <antoncotelo@gmail.com>
2024-11-27 11:59:34 -0800

    back to normal

--- example_package/data_sources.py
+++ example_package/data_sources.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import xlwings as xw
 
 
 def get_mortality_data(gender: str) -> pd.DataFrame:
@@ -10,10 +9,9 @@ def get_mortality_data(gender: str) -> pd.DataFrame:
         gender_code = "F"
     else:
         raise KeyError("You've entered a gender for which there is no table")
+
     # https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables2017.html
-    df = pd.read_csv(
+    return pd.read_csv(
         f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{gender_code}_Hist_TR2017.csv",
         skiprows=4,
     )
-    xw.view(df)
-    return df
