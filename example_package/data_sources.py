0a0d3126f9f40c2be2db4ae95bbfaafa6ef2df6c
acggit <antoncotelo@gmail.com>
2024-12-02 11:13:55 -0800

    refactored name and added xlwings

--- example_package/data_sources.py
+++ example_package/data_sources.py
@@ -1,18 +1,21 @@
 import pandas as pd
+import xlwings as xw
 
 
 def get_mortality_data(gender: str) -> pd.DataFrame:
+    """Load gender-specific data from public URL and return dataframe."""
     # Transform parameter
     if gender.lower() == "male":
-        table_id = "M"
+        gender_code = "M"
     elif gender.lower() == "female":
-        table_id = "F"
+        gender_code = "F"
     else:
         raise KeyError("You've entered a gender for which there is no table")
 
     # Get dataframe
     df = pd.read_csv(
-        f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{table_id}_Hist_TR2017.csv",
+        f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{gender_code}_Hist_TR2017.csv",
         skiprows=4,
     )
+    xw.view(df)
     return df
