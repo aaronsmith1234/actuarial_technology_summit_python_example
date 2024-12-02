bfa653712e0f3dc47a5287f2aaa986ae0c7f95ac
acggit <antoncotelo@gmail.com>
2024-12-01 21:59:36 -0800

    reset branch

--- example_package/data_sources.py
+++ example_package/data_sources.py
@@ -2,16 +2,17 @@ import pandas as pd
 
 
 def get_mortality_data(gender: str) -> pd.DataFrame:
-    """Load mortality table from public URL and return dataframe."""
+    # Transform parameter
     if gender.lower() == "male":
-        gender_code = "M"
+        table_id = "M"
     elif gender.lower() == "female":
-        gender_code = "F"
+        table_id = "F"
     else:
         raise KeyError("You've entered a gender for which there is no table")
 
-    # https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables2017.html
-    return pd.read_csv(
-        f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{gender_code}_Hist_TR2017.csv",
+    # Get dataframe
+    df = pd.read_csv(
+        f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{table_id}_Hist_TR2017.csv",
         skiprows=4,
     )
+    return df
