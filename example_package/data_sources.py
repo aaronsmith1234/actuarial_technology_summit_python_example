6683a7bea7eeb87e5fb88bdf0604fb2f3310ab39
acggit <antoncotelo@gmail.com>
2024-12-10 13:09:22 -0800

    undo for demo

--- example_package/data_sources.py
+++ example_package/data_sources.py
@@ -2,18 +2,17 @@ import pandas as pd
 
 
 def get_mortality_data(gender: str) -> pd.DataFrame:
-    """Get gender-specific data from public URL and return dataframe."""
     # Transform parameter
     if gender.lower() == "male":
-        gender_code = "M"
+        table_id = "M"
     elif gender.lower() == "female":
-        gender_code = "F"
+        table_id = "F"
     else:
         raise KeyError("You've entered a gender for which there is no table")
 
     # Get dataframe
     df = pd.read_csv(
-        f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{gender_code}_Hist_TR2017.csv",
+        f"https://www.ssa.gov/oact/HistEst/PerLifeTables/2017/PerLifeTables_{table_id}_Hist_TR2017.csv",
         skiprows=4,
     )
     return df
